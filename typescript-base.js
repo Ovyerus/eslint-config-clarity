6170ce61872ebf044c9bf4ebda0cc267dda827c8
Ovyerus <iamovyerus@gmail.com>
2019-06-22 15:01:41 +1000

    [typescript-base] Add sourceType option

--- typescript-base.js
+++ typescript-base.js
@@ -1,6 +1,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: [ '@typescript-eslint' ],
+    parserOptions: { sourceType: 'module' },
     overrides: [
         {
             files: [ '*.ts', '*.tsx' ],
