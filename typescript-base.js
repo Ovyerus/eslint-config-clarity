194d02462c8b35ed1b0a9685dc18dc6a551ff8f0
Ovyerus <iamovyerus@gmail.com>
2020-02-15 12:23:12 +1100

    [typescript] Add new rules and options
    Brings current config up-to-date with typescript-eslint v2.19.2

--- typescript-base.js
+++ typescript-base.js
@@ -13,8 +13,10 @@ module.exports = {
         "no-unreachable": "off",
         "valid-typeof": "off",
         "no-const-assign": "off",
+        "no-implied-eval": "off",
         "no-new-symbol": "off",
         "no-this-before-super": "off",
+        "no-throw-literal": "off",
         "no-undef": "off",
         "no-dupe-class-members": "off",
         "no-redeclare": "off",
@@ -46,6 +48,7 @@ module.exports = {
     "@typescript-eslint/class-name-casing": "error",
     "@typescript-eslint/consistent-type-assertions": "error",
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "@typescript-eslint/default-param-last": "error",
     "@typescript-eslint/explicit-member-accessibility": [
       "error",
       {
@@ -53,16 +56,34 @@ module.exports = {
         overrides: { parameterProperties: "explicit" }
       }
     ],
+    // Maybe this one???
+    // "@typescript-eslint/explicit-module-boundary-types": """
+
     // '@typescript-eslint/member-ordering': ''
     "@typescript-eslint/no-array-constructor": "error",
+    "@typescript-eslint/no-dupe-class-members": "error",
+    "@typescript-eslint/no-dynamic-delete": "warn",
     "@typescript-eslint/no-empty-interface": "error",
+    "@typescript-eslint/no-extra-non-null-assertion": "error",
     "@typescript-eslint/no-extraneous-class": "error",
     "@typescript-eslint/no-for-in-array": "error",
+    "@typescript-eslint/no-implied-eval": "error",
     "@typescript-eslint/no-inferrable-types": "error",
     "@typescript-eslint/no-misused-new": "error",
     "@typescript-eslint/no-namespace": "error",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
     "@typescript-eslint/no-require-imports": "warn",
     "@typescript-eslint/no-this-alias": "error",
+    "@typescript-eslint/no-throw-literal": "error",
+    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+    "@typescript-eslint/no-unnecessary-condition": [
+      "error",
+      {
+        allowConstantLoopConditions: true,
+        ignoreRhs: true,
+        checkArrayPredicates: true
+      }
+    ],
     "@typescript-eslint/no-unnecessary-qualifier": "error",
     "@typescript-eslint/no-unnecessary-type-assertion": "error",
     "@typescript-eslint/no-unused-vars": "error",
@@ -72,13 +93,22 @@ module.exports = {
     ],
     "@typescript-eslint/no-useless-constructor": "error",
     "@typescript-eslint/no-var-requires": "warn",
+    "@typescript-eslint/prefer-as-const": "error",
     "@typescript-eslint/prefer-for-of": "error",
     "@typescript-eslint/prefer-function-type": "error",
     "@typescript-eslint/prefer-includes": "error",
+    "@typescript-eslint/prefer-nullish-coalescing": "error",
+    "@typescript-eslint/prefer-optional-chain": "error",
     // '@typescript-eslint/prefer-regexp-exec': ''
     "@typescript-eslint/prefer-string-starts-ends-with": "error",
     "@typescript-eslint/require-array-sort-compare": "error",
     "@typescript-eslint/restrict-plus-operands": "error",
+    "@typescript-eslint/restrict-template-expressions": [
+      "error",
+      { allowNumber: true, allowBoolean: true }
+    ],
+    "@typescript-eslint/return-await": "error",
+    "@typescript-eslint/switch-exhaustiveness-check": "error",
     "@typescript-eslint/triple-slash-reference": [
       "error",
       { types: "prefer-import", path: "never", lib: "never" }
