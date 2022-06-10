82fd75ea8e397aee4e9be958696c3321d34c3848
Michael Mitchell <michael@ovyerus.com>
2022-06-10 12:44:12 +1000

    feat: add vue-2-typescript config

---
+++ vue-2-typescript.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  extends: ["clarity/vue-2"],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+};
